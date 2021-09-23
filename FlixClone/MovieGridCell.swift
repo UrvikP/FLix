29bab6ffcd0b13f5f67929f81df21fb1e763fdd7
UrvikP <urvik95@gmail.com>
2021-09-22 20:37:04 -0400

    finished part 2

---
+++ FlixClone/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixClone
+//
+//  Created by Urvik Patel on 9/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
